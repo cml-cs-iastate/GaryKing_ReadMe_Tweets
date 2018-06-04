d8831327cb7103b1e825adfe0eaa2e36f3192b26
Xander Apponi <xapponi@gmail.com>
2018-06-04 13:27:00 -0500

    Moved clinton demo files

---
+++ clinton_test.R
@@ -0,0 +1,9 @@
+options(repos = getOption("repos")["CRAN"])
+
+install.packages("VA", repos= "http://r.iq.harvard.edu", type="source")
+install.packages("ReadMe", repos= "http://r.iq.harvard.edu", type="source")
+
+
+library(ReadMe)
+
+#demo(clinton)
